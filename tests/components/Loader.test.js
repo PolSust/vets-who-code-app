cc15e0db51dc1d07791f0887a6098687f31582e6
Joe Romine <joe.romine@ufl.edu>
2021-08-23 20:38:06 -0700

    update loader test

--- tests/components/Loader.test.js
+++ tests/components/Loader.test.js
@@ -3,8 +3,21 @@ import { render } from '@testing-library/react'
 
 describe('<Loader />', () => {
   test('should render correctly', () => {
-    const { container } = render(<Loader />)
-    let div = container.querySelectorAll('div');
-    expect(div.length).toBe(4)
+    const { container } = render(<Loader isSubmitted={ true } jobData={ false } />)
+    let ball = container.querySelectorAll('.ball');
+    let hidden = container.querySelectorAll('.hidden');
+    expect(hidden.length).toBe(0);
+    expect(ball.length).toBe(3);
   })
+
+  test('should render correctly', () => {
+    const { container } = render(<Loader isSubmitted={ false } jobData={ true } />)
+    let loading = container.querySelectorAll('.loading');
+    let hidden = container.querySelectorAll('.hidden');
+    let ball = container.querySelectorAll('.ball');
+    expect(hidden.length).toBe(1);
+    expect(loading.length).toBe(1);
+    expect(ball.length).toBe(3);
+  })
+
 })
