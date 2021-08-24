b41531d8a444da33fdd36b8d70765b64635a9066
Joe Romine <joe.romine@ufl.edu>
2021-08-23 20:11:04 -0700

    add Card Test

---
+++ tests/components/Card.test.js
@@ -0,0 +1,45 @@
+import Card from '../../src/components/Jobs/Card/Card'
+import { render } from '@testing-library/react'
+
+
+describe('<Card />', () => {
+    let jobData ={
+        created: new Date(),
+        title: 'Test Card',
+        company: {display_name: 'Company XYZ'},
+        location: {display_name: 'Location'},
+        description: 'main card info',
+        redirect_url: 'http://example.com'
+    };
+
+  test('should render correctly', () => {
+    const { container } = render(<Card jobData={jobData} />)
+    let div = container.querySelectorAll('div');
+    let gridItem = container.querySelectorAll('.grid-item');
+    let btn = container.querySelectorAll('.btn');
+    let gridContainer = container.querySelectorAll('.grid-container');
+    expect(div.length).toBe(6);
+    expect(gridItem.length).toBe(5);
+    expect(btn.length).toBe(1);
+    expect(gridContainer.length).toBe(1);
+    console.log(container.innerHTML);
+  })
+
+});
+
+//   test('should contain class hidden when isSubmitted is true', () => {
+//     const { container } = renderIgnoringUnstableFlushDiscreteUpdates(<Video isSubmitted={true} />)
+//     const videoTag = container.querySelector('.veteran-video ')
+//     expect(videoTag.classList.contains('hidden')).toBe(true)
+//   })
+// })
+
+
+
+// describe('<Footer />', () => {
+//     test('should render correctly', () => {
+//       const { container } = render(<Footer />)
+//       expect(container.firstChild).toMatchSnapshot()
+//     })
+//   })
+  
